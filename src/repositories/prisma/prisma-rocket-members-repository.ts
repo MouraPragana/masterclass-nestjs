4f84b6e97abb17bb43817f6c34c30fadd4aa9cc9
Mateus Pragana <mateuspragana@gmail.com>
2022-12-09 21:58:08 -0300

    Finalizando masterclass

--- src/repositories/prisma/prisma-rocket-members-repository.ts
+++ src/repositories/prisma/prisma-rocket-members-repository.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { randomUUID } from 'node:crypto';
 import { PrismaService } from 'src/database/prisma.service';
 import { RocketMembersRepository } from '../rocket-members-repository';
 
+@Injectable()
 export class PrismaRocketMembersRepository implements RocketMembersRepository {
   constructor(private prisma: PrismaService) {}
 
