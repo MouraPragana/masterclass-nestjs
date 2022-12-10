fec92399106f9d941ac5821966b64dc37426c463
Mateus Pragana <mateuspragana@gmail.com>
2022-12-09 21:07:30 -0300

    Primeira hora masterclass

---
+++ src/repositories/prisma/prisma-rocket-members-repository.ts
@@ -0,0 +1,17 @@
+import { randomUUID } from 'node:crypto';
+import { PrismaService } from 'src/database/prisma.service';
+import { RocketMembersRepository } from '../rocket-members-repository';
+
+export class PrismaRocketMembersRepository implements RocketMembersRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(name: string, memberFunction: string): Promise<void> {
+    await this.prisma.rocketTeamMember.create({
+      data: {
+        id: randomUUID(),
+        name,
+        function: memberFunction,
+      },
+    });
+  }
+}
